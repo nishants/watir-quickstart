0bb0734d43ff1d3de1d18c85540ac04b93422d68
nishant <nishant.singh87@gmail.com>
2016-10-16 17:02:44 +0530

    config chrome driver.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,16 @@
+require 'watir-webdriver'
 require_relative "../pages/page"
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), "../pages")) + "/**/*.rb"].each do |file|
   require file
 end
+
+chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"drivers","chromedriver")
+Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+
+browser = Watir::Browser.new :chrome
+browser.goto("google.com")
+browser.text_field(name: 'q').set("WebDriver rocks!")
+browser.button(name: 'btnG').click
+puts browser.url
+browser.close
+$browser = browser
