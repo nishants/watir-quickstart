c88e1a3272392d9ed12f443ada48359890f38b57
nishant <nishant.singh87@gmail.com>
2016-11-29 16:23:50 +0530

    moving data inside config.

--- src/support/users.rb
+++ src/support/users.rb
@@ -3,11 +3,14 @@ require 'yaml'
 class Users
 
   def initialize
-    @users = YAML.load_file("data/#{ENV['data']}/users.yml")
+    @users = YAML.load_file(user_file)
   end
 
   def get(user_name)
     @users[user_name]
   end
 
+  def user_file
+    "#{FigNewton.data}/users.yml"
+  end
 end
