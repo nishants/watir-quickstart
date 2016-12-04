5ad4b45b66cc9a7f5ce19ea1ed69b6a7ca11483e
nishant <nishant.singh87@gmail.com>
2016-12-04 09:22:58 +0530

    pagse dir and profile parameter.

--- src/support/env.rb
+++ src/support/env.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require 'watir-webdriver'
 require 'fig_newton'
 
+DEFAULT_PAGES_DIR = "./src/pages"
 
-
-pages_dir = "../pages/bigweb"
-require_relative "#{pages_dir}/page"
+pages_dir = ENV["pages"] || DEFAULT_PAGES_DIR
+require "#{pages_dir}/page"
 
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), pages_dir)) + "/**/*.rb"].each do |file|
   require file
