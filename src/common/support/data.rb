dd5a172f99512201e3009d53408351b2eda6a224
nishant <nishant.singh87@gmail.com>
2016-12-04 11:40:58 +0530

    cleanup data suite.

--- src/common/support/data.rb
+++ src/common/support/data.rb
@@ -6,35 +6,9 @@ class DataSuite
     @path = path
   end
 
-  def timeout
-    300
-  end
-
-  def get(key)
-    data  = current[@scenario]
-    error_check(data.nil?, "No data for scenario : '#{@scenario}' in file #{data_file}")
-    data[key]
-  end
-
-  def current
-    error_check(!File.exist?(data_file), "Data file not found : #{Dir.pwd}/#{data_file}")
-    @data = @data || YAML.load_file(data_file)
-    error_check(!@data, "Data file empty : #{Dir.pwd}/#{data_file}")
-    @data
-  end
-
-  def error_check(has_error, message)
-    raise message if has_error
-  end
-
-  def data_file
-    "#{@path}/#{@feature_path}.yml"
-  end
-
   def load(feature_path, scenario_name)
-    @scenario = scenario_name
-    @feature_path = feature_path
-    @data = nil
-    OpenStruct.new(YAML.load_file(data_file)[@scenario])
+    file = "#{@path}/#{feature_path}.yml"
+    data = YAML.load_file(file)[scenario_name]
+    OpenStruct.new(data)
   end
 end
