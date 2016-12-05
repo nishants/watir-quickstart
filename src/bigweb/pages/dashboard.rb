4ddfef5722054214e214602f2bb63a347036f531
nishant <nishant.singh87@gmail.com>
2016-12-05 11:27:27 +0530

    a user table example.

--- src/bigweb/pages/dashboard.rb
+++ src/bigweb/pages/dashboard.rb
@@ -1,12 +1,16 @@
 class DashboardPage < Page
   page_url login_url
-  label(:message, :name =>  "bigweb-message")
+  label(:message,       :name =>  "bigweb-message")
+  button(:view_users,     :id =>  "current-users")
+  table(:active_users,  :id =>  "active-users")
 
   def get_user_message
     message_element.when_present.text
   end
 
   def active_users
+    view_users_element.when_present.click
+    active_users_element.when_present
     [["User One","user-one@mails.com","10 Minutes"], ["User Two","user-two@mails.com","9 hours, 10 Minutes"]]
   end
 end
