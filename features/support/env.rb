96345216234b388aa501e7c48f580dd159446b41
nishant <nishant.singh87@gmail.com>
2016-12-22 01:42:52 +0530

    shared pages.

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,10 @@ def load_pages
   ENV["url"] = FigNewton.urls.send(ENV["url"] || "default")
   require_relative "../../lib/pages/page"
 
+  Dir["./lib/pages/*.rb"].each do |file|
+    require file
+  end
+
   Dir["#{FigNewton.page_dir.send(ENV["page_dir"])}/**/*.rb"].each do |file|
     require file
   end unless ENV["page_dir"].nil?
