7a69ebee61972fc453832fb887c669532ff57c33
nishant <nishant.singh87@gmail.com>
2016-12-22 02:32:22 +0530

    refactoring rakefile.

--- lib/pages/bigweb/dashboard_page.rb
+++ lib/pages/bigweb/dashboard_page.rb
@@ -1,8 +1,8 @@
 class Dashboard < Page
   page_url login_url
-  label(:message,       :name =>  "bigweb-message")
-  link(:view_users,     :id =>  "current-users")
-  table(:users,  :id =>  "users")
+  label(:message,   :name =>  "bigweb-message")
+  link(:view_users, :id =>  "current-users")
+  table(:users,     :id =>  "users")
 
   def get_user_message
     message_element.when_present.text
